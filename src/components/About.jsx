3396b168256d048320886e6dd2198c8eb4d78eff
Nick Castagnaro <nickshs123@gmail.com>
2025-03-04 13:21:21 -0500

    Update About.jsx

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -17,7 +17,7 @@ const About = () => {
               <p>Hi. I'm Nick, nice to meet you. Please take a look around.</p>
             </div>
             <div>
-              <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+              <p>Lorem @@@@@@@@ipsum dolor, sit amet consectetur adipisicing elit.
                Qui voluptatibus fugiat quod! Dolorum asperiores libero repudiandae 
                praesentium cumque, eius pariatur accusamus, vero alias quasi ipsa 
                illo expedita, ut aperiam maiores?</p>  
